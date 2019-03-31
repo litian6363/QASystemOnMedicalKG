c5c671eb0026090a1c9d5cfbbadfce50817531fa
litian <litian@URUN.COM>
2019-03-31 17:36:27 +0800

    更新水印

--- chatbot_graph.py
+++ chatbot_graph.py
@@ -16,7 +16,7 @@ class ChatBotGraph:
         self.searcher = AnswerSearcher()
 
     def chat_main(self, sent):
-        answer = '您好，我是小勇医药智能助理，希望可以帮到您。如果没答上来，可联系https://liuhuanyong.github.io/。祝您身体棒棒！'
+        answer = '您好，我是医药智能助理，希望可以帮到您。'
         res_classify = self.classifier.classify(sent)
         if not res_classify:
             return answer
@@ -27,10 +27,11 @@ class ChatBotGraph:
         else:
             return '\n'.join(final_answers)
 
+
 if __name__ == '__main__':
     handler = ChatBotGraph()
     while 1:
         question = input('用户:')
         answer = handler.chat_main(question)
-        print('小勇:', answer)
+        print('医药智能助理:', answer)
 
